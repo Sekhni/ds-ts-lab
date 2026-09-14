b34d7957264bc0701c7bad8ff86fadbef4ada0a3
Ahmed Sekhni <20105853@mail.wit.ie>
2026-09-14 12:59:19 +0100

    Typed function signature.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,7 +1,4 @@
-// (Optional) Change the names below to your friends.
-import {Friend, Colleague, ColleagueHistory} from "./myTypes";
-
-
+import { Friend, Colleague, ColleagueHistory } from './myTypes'
 
 const friend1: Friend = {
   name: "Paul Fleming",
@@ -16,7 +13,7 @@ const friend2: Friend = {
 };
 
 export const friends = [friend1, friend2];
-//console.log(friends[1]);
+// console.log(friends[1]);
 
 //   -------------------
 const colleague1: Colleague = {
@@ -45,9 +42,10 @@ const colleague3: Colleague = {
     extension: 125,
   },
 };
+
 export const colleagues: ColleagueHistory = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
 
-//console.log(colleagues.current[0]);//
+// console.log(colleagues.current[0]);
